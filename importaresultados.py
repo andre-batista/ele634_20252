05147af517db9032d0fbbb5146c5ecfd4edab7d2
André Batista <andre-costa@ufmg.br>
2025-12-04 17:56:43 -0300

    Corrigindo importação dos resultados

--- importaresultados.py
+++ importaresultados.py
@@ -1,4 +1,13 @@
+import os
 from gdown import download_folder
 
-url = "https://drive.google.com/drive/folders/1lx5mHSLd7cfd9FpHnj2Vj6eDz7xYJVk-?usp=sharing"
-download_folder(url, quiet=False, use_cookies=False)
+os.makedirs(f'./resultados', exist_ok=True)
+links = {"pequena": "https://drive.google.com/drive/folders/1Zpu57kWp7bXxLCcOeVg17oxW6AwYpl19?usp=sharing",
+         "media": "https://drive.google.com/drive/folders/1wneL7z_ynzNmQtStXWmzTWc5fYqlOUxV?usp=sharing",
+         "grande": "https://drive.google.com/drive/folders/1y6F04vQRlqW8pViHc2aXwYIetgNwy27X?usp=sharing",
+         "rush": "https://drive.google.com/drive/folders/1FmIERZPkGsh-Ju7JHtP65ovM_0vAzjtF?usp=sharing"}
+
+for instancia, url in links.items():
+    os.makedirs(f'./resultados/{instancia}', exist_ok=True)
+    download_folder(url, output=f'./resultados/{instancia}', quiet=False, 
+                    use_cookies=True)
